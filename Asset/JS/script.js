50d85a45b3e7565e042f14f93d40c2793f8fe06f
playground <connie.s.barrantes@gmail.com>
2021-12-21 21:04:49 -0500

    displayed current data and 5 day forecast on screen such as temp, humidity, windspeed and UVI

--- Asset/JS/script.js
+++ Asset/JS/script.js
@@ -1,5 +1,11 @@
 // GIVEN a weather dashboard with form inputs
 var weather;
+var currentDayCity = document.querySelector('#currentCity');
+var currentTemp = document.querySelector('#temp');
+var currentHumidity = document.querySelector('#humidity');
+var currentWindSpeed = document.querySelector('#windSpeed');
+var currentUVIndex = document.querySelector('#uvIndex');
+var currentWeatherIcon = document.querySelector("#weatherIcon")
 
 var api = 'https://api.openweathermap.org/data/2.5/weather?q='
 var apiKey = 'aefb5a0c0b478f7f82221b45c7eddcc8'
@@ -8,41 +14,113 @@ var units = '&units=imperial'
 $(document).ready(function() {
   $("#submit").click(weatherSearch);
 
-  function revealMessage() {
-    document.getElementById("hiddenMessage").style.display = "block";
-  }
+  // function revealMessage() {
+  //   document.getElementById("hiddenMessage").style.display = "block";
+  // }
 
   function displayWeather(data) {
     for (var i = 0; i < data.length; i++) {
-      var {name} = data;
-      
+      var button = select('#submit');
+      button.onclick(weatherSearch);
+      input = select('#city');
+      console.log(city)
     }
     
   }
 
   function weatherSearch() {
-    revealMessage()
+  
     var searchFieldInput = $("#searchField").val()
+           
+
             fetch(
-            "https://api.openweathermap.org/data/2.5/weather?q=" +
-            searchFieldInput +
+            "https://api.openweathermap.org/data/2.5/weather?q=atlanta" +
+            // searchFieldInput +
               "&units=metric&appid=" +
               apiKey
           )
             .then((response) => {
+              // console.log(response);
               if (!response.ok) {
                 alert("No weather found.");
                 throw new Error("No weather found.");
-              }
+              } 
               return response.json();
-              console.log(response)
+             
             })
             .then((data) => {
-              displayWeather(data)
+              var fiveDayData = fetch5Days(data.coord.lat,data.coord.lon);
               console.log(data);
+              document.getElementById("hiddenMessage").style.display = "block";
+
+              console.log("temp response: ",data);
+              currentDayCity.textContent = data.name;
+              currentTemp.textContent = `${data.main.temp} F`;
+              currentHumidity.textContent = `Humidity: ${data.main.humidity}`;
+              currentWindSpeed.textContent = `Wind speed: ${data.wind.speed}`;
+              
+              console.log(fiveDayData);
+              
+              displayWeather(data)
             });
             
     }
+
+    function fetch5Days (lat,lon) {
+      fetch (
+          `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={part}&units=imperial&appid=${apiKey}`
+        )
+        .then((response) => {
+          // console.log(response);
+          if (!response.ok) {
+            alert("No weather found.");
+            throw new Error("No weather found.");
+          } 
+          return response.json();
+         
+        })
+        .then((data) => {
+          console.log(data);
+          currentUVIndex.textContent = `UVI: ${data.current.uvi}`;
+
+          for (var i=1; i < 6; i++) {
+            console.log(data.daily[i].temp.day);
+            document.querySelector("#temp-" + i).textContent=`${data.daily[i].temp.day} F`;
+            document.querySelector("#humidity-" + i).textContent=`Humidity: ${data.daily[i].humidity}`;
+            document.querySelector("#windSpeed-" + i).textContent=`Wind speed: ${data.daily[i].wind_speed}`;
+          }
+        });
+      
+    }
+
+
+
+// weatherSearch()
+
+  // function weatherSearch() {
+  //   revealMessage()
+  //   var searchFieldInput = $("#searchField").val()
+  //           fetch(
+  //           "https://api.openweathermap.org/data/2.5/weather?q=" +
+  //           searchFieldInput +
+  //             "&units=metric&appid=" +
+  //             apiKey
+  //         )
+  //           .then((response) => {
+  //             if (!response.ok) {
+  //               alert("No weather found.");
+  //               throw new Error("No weather found.");
+  //             }
+  //             console.log(response)
+  //             return response.json();
+  //           })
+  //           .then((data) => {
+  //             displayWeather(data)
+  //             console.log(data);
+  //           });
+            
+  //   }
+  //   weatherSearch()
 
     
 
